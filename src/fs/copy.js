8a3b599253a85dfd004b64f84ecde53036ea3619
priteev <alexander.priteev@sibers.com>
2024-01-23 17:56:36 +0600

    feat: add src/fs - copy.js solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { fileURLToPath  } from 'node:url';
+import {dirname, join} from 'node:path';
+import { cp, access } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const isCreate = async (path) => {
+    try {
+        await access(path);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
 const copy = async () => {
-    // Write your code here 
+    const PATH_CURRENT = join(dirname(__filename), 'files');
+    const PATH_NEW = join(dirname(__filename), 'files_copy');
+    const isCurrent = await isCreate(PATH_CURRENT);
+    const isNew = await isCreate(PATH_NEW);
+    if(!isCurrent || isNew)  throw new Error('FS operation failed');
+    await cp(PATH_CURRENT, PATH_NEW, { recursive: true });
 };
 
 await copy();
