3d529ab1c99e2555b0073a8c165ca96da499385c
priteev <alexander.priteev@sibers.com>
2024-01-23 18:06:29 +0600

    feat: add src/fs - rename.js solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import { fileURLToPath  } from 'node:url';
+import {dirname, join} from 'node:path';
+import { rename as renameFile, access } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const isCreate = async (path) => {
+    try {
+        await access(path);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
 const rename = async () => {
-    // Write your code here 
+    const PATH_WRONG = join(dirname(__filename), 'files/wrongFilename.txt');
+    const PATH_PROPER = join(dirname(__filename), 'files/properFilename.md');
+    const isCurrent = await isCreate(PATH_WRONG);
+    const isNew = await isCreate(PATH_PROPER);
+    if(!isCurrent || isNew)  throw new Error('FS operation failed');
+    await renameFile(PATH_WRONG, PATH_PROPER)
 };
 
 await rename();
