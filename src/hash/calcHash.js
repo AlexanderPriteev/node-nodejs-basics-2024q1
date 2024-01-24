d7e0f88ee21a5b6fd7a5bf42a60d01f43e51e7db
priteev <alexander.priteev@sibers.com>
2024-01-24 17:53:34 +0600

    feat: add src/hash - calcHash.js solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { fileURLToPath  } from 'node:url';
+import {dirname, join} from 'node:path';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import { createHash } from 'node:crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const PATH = join(dirname(__filename), 'files/fileToCalculateHashFor.txt');
+    const stream = createReadStream(PATH);
+    stream.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
