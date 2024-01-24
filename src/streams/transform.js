c5444b777d3dd263dff00310ecbd2b4b8709be46
priteev <alexander.priteev@sibers.com>
2024-01-24 18:32:05 +0600

    feat: add src/streams - transform.js solution

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
-const transform = async () => {
-    // Write your code here 
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
+const textReverse = new Transform({
+    transform(chunk, encoding, callback) {
+        callback(null, `${[...chunk.toString()].reverse().join('')}\n`);
+    },
+});
+
+const transform = () => {
+    stdin.pipe(textReverse).pipe(stdout);
 };
 
-await transform();
+transform();
