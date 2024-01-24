84fcae6d7806ba2f3d2e4a5357012977ea8c9262
priteev <alexander.priteev@sibers.com>
2024-01-24 16:25:21 +0600

    feat: add src/cli - args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const list = [];
+    for(let i = 0; i < argv.length; i += 1){
+       if(/^--/.test(argv[i])) {
+           list.push(`${argv[i].replace('--', '')} is ${argv[i + 1]}`);
+           i += 1;
+       }
+    }
+    console.log(list.join(', '));
 };
 
 parseArgs();
