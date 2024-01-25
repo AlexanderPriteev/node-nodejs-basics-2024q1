f201e39982aa3dd43103cc5c3d5b4b9409af9b90
priteev <alexander.priteev@sibers.com>
2024-01-25 14:04:32 +0600

    feat: add src/zip - decompress.js solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { fileURLToPath  } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createWriteStream, createReadStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+    const txtPath = join(dirname(__filename), 'files/fileToCompress.txt');
+    const gzPath = join(dirname(__filename), 'files/archive.gz');
+    const streamRead = createReadStream(gzPath);
+    const streamWrite = createWriteStream(txtPath);
+    const decompress = createUnzip();
+    await streamRead.pipe(decompress).pipe(streamWrite);
 };
 
 await decompress();
